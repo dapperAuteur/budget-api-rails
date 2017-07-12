8c0fdc3ed570dda82948369fe884aaa231daf609
Anthony McDonald <a@awews.com>
2017-07-12 13:10:29 -0700

    add index tests and another show test

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -14,4 +14,22 @@ describe Api::V1::UsersController do
       expect(response.code).to eq('200')
     end
   end
+
+  describe "GET #index" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      auth_headers = @user.create_new_auth_token
+      request.headers.merge!(auth_headers)
+      get :index, params: { id: @user.id }
+    end
+
+    it 'responds with 200 status code' do
+      expect(response.code).to eq('200')
+    end
+
+    it 'returns the serialized user attributes' do
+      expect(JSON.parse(response.body)['data'].length).to eq(1)
+      expect(JSON.parse(response.body)['data'].first['attributes']).to eq({'name'=>'aweful brown', 'email'=>'aweful@aweful.com'})
+    end
+  end
 end
